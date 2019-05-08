f560aa8a6d5e6688e8127e6e8cc50ecc6e08dea0
vinicius pulgatti <vini2019ide@gmail.com>
2019-05-08 10:43:29 -0300

    adicionar projeto testando.group e teste.java.

---
+++ teste.java
@@ -0,0 +1,45 @@
+package test;
+
+
+@Entity
+public class Pessoas {
+
+
+    Long id;
+    String nome;
+    Long cpf;
+    Long rg;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getRg() {
+        return rg;
+    }
+
+    public void setRg(Long rg) {
+        this.rg = rg;
+    }
+}
