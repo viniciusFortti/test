426402d1dfc7c9372cd7cd1db335025ddaa57950
vinicius pulgatti <vini2019ide@gmail.com>
2019-05-17 16:00:46 -0300

    adicionar arquivos sexta-feira.

--- OrientandoAObjetos/src/main/java/br/com/vinicius/Semana2/Vetores/Main.java
+++ OrientandoAObjetos/src/main/java/br/com/vinicius/Semana2/Vetores/Main.java
@@ -4,57 +4,30 @@ import br.com.vinicius.Semana2.*;
 
 public class Main {
 
+    public static void main(String[] args) {
 
-        /*double[] temperaturas = new double[10];
-
-        double temperaturaDia2 = 25.6;
-        double temperaturaDia3 = 17.5;
-        double temperaturaDia4 = 35.7;
-        double temperaturaDia1 = 30.1;
-        double soma1 = 0;
-        double soma = 0;
-        temperaturas[0] = 15.7;
-        temperaturas[1] = temperaturaDia2;
-        temperaturas[2] = temperaturaDia3;
-        temperaturas[3] = temperaturaDia4;
-
-        VetoresAula1 vetor = new VetoresAula1();
-
-        vetor.ImprimiVetor();
-
-        for (int i = 0; i < temperaturas.length; i++) {
-            System.out.println("temperatura do dia " + (i + 1) + " é : " + temperaturas[i]);
-        }
-
-        for (int i = 0; i < (temperaturas.length - 1); i++) {
-
-            soma1 += temperaturas[i];
-
-        }
-        System.out.println("a soma dos valores é : " + soma1);
-
-        //CLONAR UM VETOR.
-        double[] clone = temperaturas.clone();
-
-        for (double valor : clone) {
-            System.out.println(valor);
-        }
-        for (double valor : clone) {
-            soma += valor;
-
-        }System.out.println(soma);
+        VetoresAula1 vetor = new VetoresAula1(3);
 
 
-        int[][] matriz= {{1,2,3},{4,5,6},{7,8,9}} ;
+            vetor.adiciona("a");
+            vetor.adiciona("b");
+            vetor.adiciona("c");
+            vetor.adiciona("c");
+            vetor.adiciona("c");
+            vetor.adiciona("s");
+            vetor.adiciona("f");
+            vetor.adiciona("fc");
 
-        System.out.println(matriz.length);
+            vetor.ImprimirVetor();
 
-        for(int linha = 0;linha < matriz.length; linha++) {
-            for(int coluna = 0;coluna < matriz.length; coluna++) {
-                System.out.println(matriz[linha][coluna]);
-            }
-        }
-*/
+            vetor.ImprimirTamanho();
+
+            vetor.toString();
+
+
+
+
+    }
 }
 
 
--- OrientandoAObjetos/src/main/java/br/com/vinicius/Semana2/Vetores/VetoresAula1.java
+++ OrientandoAObjetos/src/main/java/br/com/vinicius/Semana2/Vetores/VetoresAula1.java
@@ -1,15 +1,87 @@
 package br.com.vinicius.Semana2.Vetores;
 
 
+import java.util.Arrays;
 
 public class VetoresAula1 {
 
-    double[] temperaturas = new double[10];
+    private String[] elementos = new String[10];
+
+    private int tamanho;
+
+    public VetoresAula1 (int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+   /* public void adiciona(String elemento){
+        for(int i = 0; i < elementos.length; i++){
+            if (this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    }*/
+
+ /*  public void adiciona(String elemento) throws Exception {
+       if(this.tamanho > this.elementos.length) {
+        this.elementos[this.tamanho] = elemento;
+        this.tamanho++;
+       }
+
+       else {throw new Exception("Esta cheio");}
+
+   }
+*/
 
 
-    public void ImprimiVetor(){
-        for (int i = 0; i < temperaturas.length; i++){
-            System.out.println("temperatura do dia " + (i+1) + " é : " + temperaturas[i]);
+    public boolean adiciona(String elemento) {
+        if(this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho ++;
+            return true;
+        }else{
+            return false;
         }
     }
+
+
+    public boolean adiciona(String elemento, int posicao) {
+
+
+        if(!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("posição nao existe");
+
+        }
+        for (int i = tamanho - 1; i >= posicao; i--){
+            this.elementos[i + 1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+         return true;
+    }
+
+
+    public void ImprimirVetor(){
+        for (int i=0;i < this.elementos.length; i++) {
+            System.out.println(elementos[i]);
+        }
+
+    }
+
+    public int ImprimirTamanho(){
+
+            return this.tamanho;
+
+    }
+
+    @Override
+    public String toString() {
+        return "VetoresAula1{" +
+                "elementos=" + Arrays.toString(elementos) +
+                ", tamanho=" + tamanho +
+                '}';
+    }
 }
+
