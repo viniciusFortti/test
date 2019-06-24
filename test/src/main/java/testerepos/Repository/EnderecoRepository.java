257f95c02eaff7d9082f168e0ea284e24bc49a4a
Your Namevinicius <viniciusFortti>
2019-06-24 11:22:35 -0300

    adicionar alterações.

--- test/src/main/java/testerepos/Repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package testerepos.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import testerepos.Entity.Endereco;
-
-public interface EnderecoRepository extends CrudRepository <Endereco , Integer> {
-}
